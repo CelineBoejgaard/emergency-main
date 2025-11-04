0b050e514edead78b51137edc3af8b5f6480da99
celine boejgaard <cebo1000@stud.ek.dk>
2025-11-04 08:39:37 +0100

    3commit

--- js/instruction.js
+++ js/instruction.js
@@ -4,10 +4,10 @@ document.querySelector(".knap1").addEventListener("click", knap1);
 function knap1() {
   document.querySelector("#brilleglas").classList.add("my_scale");
   document.querySelector("#brilleglas").style.fill = "red";
-}
 
-document.querySelector(".knap2").addEventListener("click", knap2);
+  document.querySelector(".info-text h2").textContent =
+    "Solbriller er ikke længere vigtig";
 
-function knap2() {
-  document.querySelector("solhat").classList.add("my_scale");
+  document.querySelector(".placeholder").textContent =
+    "Der er ingen sol, så du behøver ikke solbriller";
 }
