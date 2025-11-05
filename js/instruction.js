4df0836434c6a42b2c724748ef07484ed305454e
celine boejgaard <cebo1000@stud.ek.dk>
2025-11-05 20:31:10 +0100

    4commit

--- js/instruction.js
+++ js/instruction.js
@@ -11,3 +11,10 @@ function knap1() {
   document.querySelector(".placeholder").textContent =
     "Der er ingen sol, så du behøver ikke solbriller";
 }
+
+document.querySelector(".knap2").addEventListener("click, knap2");
+
+function knap2() {
+  document.querySelector("#_solhat_").classList.add("my_scale");
+  document.querySelector("#_solhat_").style.fill = "red";
+}
